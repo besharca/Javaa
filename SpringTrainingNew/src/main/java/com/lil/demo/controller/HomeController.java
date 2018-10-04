68472a989259cb19023bffede6de6c7f2c6b7fb6
besharca <puiugeorgealexandru@yahoo.com>
2018-10-04 18:30:04 +0300

    saved 6.30

---
+++ SpringTrainingNew/src/main/java/com/lil/demo/controller/HomeController.java
@@ -0,0 +1,38 @@
+package com.lil.demo.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.lil.demo.model.Employee;
+import com.lil.demo.repository.EmployeeRepository;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	EmployeeRepository empRepo;
+	
+	@GetMapping("home")
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView("home.jsp");
+		mv.addObject("WelcomeMessage", "Hi there bonobo");
+		return mv;
+	}
+	
+	@GetMapping("rusnak")
+	public ModelAndView huh(HttpServletRequest request) {
+		Employee result = empRepo.findById(Integer.parseInt(request.getParameter("id"))).orElse(new Employee(-1,"not found","unluck"));
+		ModelAndView mv = new ModelAndView("home.jsp");
+		String  message = result.getId() + " "+result.getName()+" "+result.getDescription();
+		System.out.println(message);
+		mv.addObject("WelcomeMessage", message);
+		
+		return mv;
+		
+	}
+	
+}
