d109bf557557ce09594cbd3e169ad41e926c6768
besharca <puiugeorgealexandru@yahoo.com>
2018-10-04 18:23:37 +0300

    ????

---
+++ SpringTrainingNew/src/main/java/com/lil/demo/SpringTrainingNewApplication.java
@@ -0,0 +1,12 @@
+package com.lil.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrainingNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTrainingNewApplication.class, args);
+	}
+}
